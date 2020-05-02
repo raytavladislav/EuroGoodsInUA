c0c203c9152e79b5e2b812c7fc49bc51e0acd9d2
Rayta Vladislav <vlad23051@gmail.com>
2020-05-02 19:34:23 +0300

    made slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsComponent } from './pages/products/products.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './pages/products/carousel/carousel.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +19,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     HeaderComponent,
     FooterComponent,
     NavbarComponent,
-    ProductsComponent
+    ProductsComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
